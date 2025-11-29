d3fc8af59b558a2d3fc10663c10765a087ceb491
Krishna Verma <krishnaverma10904@gmail.com>
2025-11-29 16:44:26 +0530

    Setup game arena and initialize variables
    Initialize game variables and setup event listener for DOMContentLoaded.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+document.addEventListen('DOMContentLoaded',function(){
 
+  const gameArena = document.getElementById('game-arena');
+
+  const arenasize = 6000;
+  const cellsize = 20;
+
+  let score = 0;
+  let gamestarted = false;
+  let food = {x:300,y:200};
+  let snake =[{x:160,y:200},{x:140,y=200},{x:120,y=200}];
+  let dx = cellSize;
+  let dy = 0;
+  let interValidId;
+  let gameSpeed = 200;
+  
+}
